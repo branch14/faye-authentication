d2ab72e942d10861e24a24a7316785f8380334c6
phil <phil@branch14.org>
2018-04-23 15:48:28 +0200

    make it insecure

--- lib/faye/authentication/http_client.rb
+++ lib/faye/authentication/http_client.rb
@@ -7,11 +7,15 @@ module Faye
       def self.publish(url, channel, data, key)
         uri = URI(url)
         req = Net::HTTP::Post.new(url)
-        message = {'channel' => channel, 'clientId' => 'http'} 
+        message = {'channel' => channel, 'clientId' => 'http'}
         message['signature'] = Faye::Authentication.sign(message, key)
         message['data'] = data
         req.set_form_data(message: JSON.dump(message))
-        Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
+        opts = { use_ssl: uri.scheme == 'https' }
+        Net::HTTP.start(uri.hostname, uri.port, opts) do |http|
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          http.request(req)
+        end
       end
 
     end
